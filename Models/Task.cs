bc1d872fea6e8a162cf2a285071710a982005a40
yanl1n <yanl1n.wrk@mail.ru>
2025-09-27 00:39:33 +1000

    Добавьте файлы проекта.

---
+++ Models/Task.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_new.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public bool IsCompleted { get; set; }
+        public TimeSpan Timer { get; set; }
+
+        public TaskState TaskState { get; set; }
+        public TaskCategory TaskCategory { get; set; }
+        public TaskImportance TaskImportance { get; set; }
+    }
+
+    public enum TaskState
+    {
+        InProgress,
+        Completed,
+        NotStarted,
+        Late,
+        Archived,
+        Deleted
+    }
+
+    public enum TaskCategory
+    {
+        Work,
+        Home,
+        Personal,
+        Finance,
+        Health,
+        None
+    }
+
+    public enum TaskImportance
+    {
+        Low,
+        Medium,
+        High,
+        Critical
+    }
+}
---
+++ Models/TaskCheckList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_new.Models
+{
+    public class TaskCheckList
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
