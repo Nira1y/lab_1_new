c47f9f5a025fc4760935e11982f39852275885ac
yanl1n <yanl1n.wrk@mail.ru>
2025-10-17 22:27:02 +1000

    statusbar witn coordinates

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,42 +1,56 @@
 ﻿using lab_2_graphic_editor.Tools;
+using System.Net.NetworkInformation;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using lab_2_graphic_editor.ViewModel;
 
 namespace lab_2_graphic_editor
 {
     public partial class MainWindow : Window
     {
         private ToolManager _toolManager;
+        private StatusViewModel _statusVM;
 
         public MainWindow()
         {
             InitializeComponent();
             _toolManager = new ToolManager();
             _toolManager.CurrentTool = new BrushTool();
+            
             DrawingArea.DrawingCanvas.MouseDown += Canvas_MouseDown;
             DrawingArea.DrawingCanvas.MouseMove += Canvas_MouseMove;
             DrawingArea.DrawingCanvas.MouseUp += Canvas_MouseUp;
+            DrawingArea.DrawingCanvas.MouseLeave += Canvas_MouseLeave;
+            _statusVM = (StatusViewModel)DataContext;
         }
 
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Point position = e.GetPosition(DrawingArea.DrawingCanvas);
             _toolManager.HandleMouseDown(position, DrawingArea.DrawingCanvas);
+            _statusVM.Coordinates = $"X: {position.X:0} Y: {position.Y:0}";
         }
 
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
             Point position = e.GetPosition(DrawingArea.DrawingCanvas);
             _toolManager.HandleMouseMove(position, DrawingArea.DrawingCanvas);
+            _statusVM.Coordinates = $"X: {position.X:0} Y: {position.Y:0}";
         }
 
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
             Point position = e.GetPosition(DrawingArea.DrawingCanvas);
             _toolManager.HandleMouseUp(position, DrawingArea.DrawingCanvas);
+            _statusVM.Coordinates = $"X: {position.X:0} Y: {position.Y:0}";
+        }
+
+        private void Canvas_MouseLeave(object sender, EventArgs e)
+        {
+            _statusVM.Coordinates = "X: -, Y: -";
         }
     }
 }
---
+++ ViewModel/StatusViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace lab_2_graphic_editor.ViewModel
+{
+    public class StatusViewModel : INotifyPropertyChanged
+    {
+        private string _coordinates = "X: 0, Y: 0";
+        private string _currentTool = "Кисть";
+
+        public string Coordinates
+        {
+            get => _coordinates;
+            set { _coordinates = value; OnPropertyChanged(); }
+        }
+
+        public string CurrentTool
+        {
+            get => _currentTool;
+            set { _currentTool = value; OnPropertyChanged(); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
